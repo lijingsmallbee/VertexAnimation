10f0a02e6fe47932d3ae37213307019028e7d6b8
nan.bai <nan.bai@funplus.com>
2018-05-30 20:59:41 +0800

    开始写运行时库

---
+++ Assets/Scripts/VertexAnimationManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class AnimationDataManager
+{
+    Dictionary<string, VertexAnimationData> _allAnimationData = new Dictionary<string, VertexAnimationData>();
+    static AnimationDataManager _Instance = null;
+    static public AnimationDataManager Instance
+    {
+        get
+        {
+            if(_Instance == null)
+            {
+                _Instance = new AnimationDataManager();
+            }
+            return _Instance;
+        }
+    }
+    // Use this for initialization
+    public VertexAnimationData GetAnimationData(string animName)
+    {
+        VertexAnimationData data = null;
+        _allAnimationData.TryGetValue(animName, out data);
+        return data;
+    }
+}
